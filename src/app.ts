6034adf190444ecf4d182278c31bfc3a383ac9f1
Jefferson <jefferson.aguilar95@gmail.com>
2020-10-28 01:10:09 -0300

    feat(server): rate limiter implemented

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import apolloServer from './graphqlServer';
 
 export const GRAPHQL_PATH = '/graphql';
 
 const app = express();
 
-apolloServer.applyMiddleware({ app, path: GRAPHQL_PATH });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('trust proxy', true);
+
+apolloServer.applyMiddleware({ app, path: GRAPHQL_PATH });
 
 app.get('*', (req, res) => {
   res.redirect(GRAPHQL_PATH);
--- src/graphqlServer.ts
+++ src/graphqlServer.ts
@@ -1,6 +1,9 @@
 import path from 'path';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import redis from 'redis';
+import mongoose from 'mongoose';
+import { ApolloError } from 'apollo-server-express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
 import { mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { ApolloServer } from 'apollo-server-express';
@@ -10,6 +13,8 @@ import { DIRECTIVES } from '@graphql-codegen/typescript-mongodb';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
 dotenv.config();
+
+const redisClient = redis.createClient({ enable_offline_queue: false });
 
 mongoose.connect(process.env.MONGO_DB_URL || 'mongodb://localhost:27017/cinema', {
   useUnifiedTopology: true,
@@ -30,13 +35,23 @@ const schemaWithResolvers = addResolversToSchema({
   resolvers: mergeResolvers(resolversArray)
 });
 
+const rateLimiter = new RateLimiterRedis({ storeClient: redisClient, points: 3000, duration: 3600 });
+
 // Initialize Apollo server
 const apolloServer: ApolloServer = new ApolloServer({
   schema: schemaWithResolvers,
   playground: {
     endpoint: '/graphql'
   },
-  introspection: true
+  introspection: true,
+  context: async ({ req }) => {
+    try {
+      await rateLimiter.consume(req.ip, 1);
+    } catch (e) {
+      console.log('e----', e);
+      throw new ApolloError('You exceed the requests limit for today, see you tomorrow!');
+    }
+  }
 });
 
 export default apolloServer;
